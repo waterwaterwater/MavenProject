2711a04125646dcf1ea13996436722ebf08805a5
Dmitry <waterwaterwater@users.noreply.github.com>
2021-03-26 19:54:10 +0300

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -17,5 +17,12 @@ public class Greeter {
  **/
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
+    
+    
+    
+    
+    
+    
+    
   }
 }

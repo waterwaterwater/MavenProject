21a99d7070d2ebb1d98a7ffcf21991b1dc7ae898
AlexSpirin1990 <spirin.am1990@gmail.com>
2021-03-28 12:25:28 +0300

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -18,14 +18,5 @@ public class Greeter {
  **/
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
-    
-    
-    
-    
-    
-    
-    
-    
-    
   }
 }
